8cadd53de044b5698f2bba5df1f9408b9b8b9cac
adelay <miginiesta@gmail.com>
2013-02-26 09:19:26 +0000

    Primera Version

---
+++ proyeMiguel/src/persona.java
@@ -0,0 +1,28 @@
+
+/*
+ * Autor Miguel iniesta Garcia
+ */
+public class persona {
+	
+	String nombre;
+	String apellido;
+	int edad;
+	
+	/*
+	 * devuelve el nombre
+	 */
+	public String getNombre()
+	{
+		return nombre;
+	}
+	
+	/*
+	 * metodo que devuelve el apellido
+	 */
+	public String getApellido()
+	{
+		return apellido;
+	}
+	
+
+}
